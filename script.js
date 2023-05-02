3b84f8c4241e03bc71d7c4b712ce2f680dd9a525
mickeysrishti <Mukeshpathak345@gmail.com>
2023-05-02 22:11:20 +0530

    coding Added

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".mainHeroSection", {
+    opacity: 0,
+    y: 500,
+    scale: 0
+})
+gsap.to(".mainHeroSection", {
+    delay: 1,
+    y: 1,
+    scale: 1,
+    opacity: 1
+})
+
+
+ 
+
